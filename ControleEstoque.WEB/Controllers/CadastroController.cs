21bbd5a38561994dbe3d63287ef865887fa3733f
André Ferrari Picolli <picolli@outlook.com.br>
2021-01-25 10:35:40 -0300

    Initial commit

---
+++ ControleEstoque.WEB/Controllers/CadastroController.cs
@@ -0,0 +1,120 @@
+﻿using ControleEstoque.WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.WEB.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel(){Id=1, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel(){Id=2, Nome="Mouses", Ativo=true },
+            new GrupoProdutoModel(){Id=3, Nome="Processadores", Ativo=true },
+            new GrupoProdutoModel(){Id=4, Nome="Monitores", Ativo=false },
+            new GrupoProdutoModel(){Id=5, Nome="Teclados", Ativo=false },
+            new GrupoProdutoModel(){Id=6, Nome="Gabinetes", Ativo=true }
+        };
+
+        [Authorize]
+        public ActionResult GrupoProduto()
+        {
+            return View(_listaGrupoProduto);
+        }
+        [HttpPost]
+        [Authorize]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(_listaGrupoProduto.Find(x => x.Id == id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            var ret = false;
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == id);
+            if (registroBD != null)
+            {
+                _listaGrupoProduto.Remove(registroBD);
+                ret = true;
+
+            }
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == model.Id);
+            if (registroBD == null)
+            {
+                registroBD = model;
+                registroBD.Id = _listaGrupoProduto.Max(x => x.Id) + 1;
+                _listaGrupoProduto.Add(registroBD);
+            }
+            else
+            {
+                registroBD.Nome = model.Nome;
+                registroBD.Ativo = model.Ativo;
+            }
+            return Json(registroBD);
+        }
+
+        [Authorize]
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult UnidadeMedida()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Pais()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Estado()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ControleEstoque.WEB/Controllers/ContaController.cs
@@ -0,0 +1,59 @@
+﻿using ControleEstoque.WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ControleEstoque.WEB.Controllers
+{
+    public class ContaController : Controller
+    {
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var admin = UsuarioModel.ValidarUsuario(login.Usuario, login.Senha);
+
+            if (admin)
+            {
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login Inválido.");
+            }
+
+            return View(login);
+
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult logOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
