71034d06b0d47143471cc4b2acf1f878f6f97549
André Ferrari Picolli <53264087+dreanp@users.noreply.github.com>
2021-01-25 10:52:50 -0300

    Update WEB to Local, teste

--- ControleEstoque.WEB/Models/LoginViewModel.cs
+++ ControleEstoque.WEB/Models/LoginViewModel.cs
@@ -9,13 +9,13 @@ namespace ControleEstoque.WEB.Models
     public class LoginViewModel
     {
         [Required(ErrorMessage ="Informe o usuário")]
-        [Display(Name = "Usuário: ")]
+        [Display(Name = "Usuário:")]
         public string Usuario { get; set; }
         [Required(ErrorMessage ="Informe a senha")]
         [DataType(DataType.Password)]
-        [Display(Name = "Senha: ")]
+        [Display(Name = "Senha:")]
         public string Senha { get; set; }
-        [Display(Name = "Lembrar Me: ")]
+        [Display(Name = "Lembrar Me:")]
         public bool LembrarMe { get; set; }
     }
 }
